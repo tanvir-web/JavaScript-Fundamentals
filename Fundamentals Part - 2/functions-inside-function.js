09f8e68e619e4ca5c0dfbf96ce8be87ac93f7973
Tanvir Ahmed <72856097+tanvir-web@users.noreply.github.com>
2021-05-23 21:45:52 +0600

    Create functions-inside-function.js

---
+++ Fundamentals Part - 2/functions-inside-function.js
@@ -0,0 +1,20 @@
+/*
+
+Fruit Processor
+
+*/
+
+
+function fruitProcessor(mango, orange){
+	const mangoSlice = fruitSlicer(mango);
+	const orangeSlice = fruitSlicer(orange);
+
+	const juice = `We are making juice of mango (${mangoSlice}) and ornage (${orangeSlice})`;
+	return juice;
+}
+
+function fruitSlicer(fruit){
+	return (fruit * 4);
+}
+
+console.log(fruitProcessor(2, 4));
